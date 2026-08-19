efc1630aafc7169a492f793438590d55ca665189
Satya Karthikeya <cb.ai.u4aid23128@cb.students.amrita.edu>
2026-08-19 23:42:42 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        braket = { ")" : "(", "]" : "[", "}" : "{" }
+
+        stack = []
+        for st in s:
+            if st in braket:
+                if stack and stack[-1]== braket[st]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(st)
+        return not stack
